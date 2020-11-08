fa22ba8ad61cbc71a3b3116fb584446ec2582dd4
shenbinghui <657699272@qq.com>
2020-11-08 12:30:57 +0800

    增加说明文件，vue3.0使用引入自定义创建的ui组件库有问题，待研究

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
+import { Vue } from 'vue-class-component'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@shenbinghui/vue-ui-components-shenbinghui-test'
 
 createApp(App).use(store).use(router).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" style='width:300px;height:400px' src="../assets/test.jpg">
+    <h2>沈沁瑶 爱吃蔬菜和水果 哈哈 ！</h2>
+    <h2 style='color:#000'>沈沁瑶老师在床上蹦蹦跳跳！</h2>
+    <Test></Test>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
   </div>
 </template>
 
@@ -9,10 +12,15 @@
 import { Options, Vue } from 'vue-class-component'
 import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
+
 @Options({
   components: {
     HelloWorld
   }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  mounted(){
+
+  }
+}
 </script>
